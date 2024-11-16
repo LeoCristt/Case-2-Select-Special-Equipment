fe937e3661220e388626590c19c3427b9fc51038
LeoCristt <SergeySav009@yandex.ru>
2024-11-16 20:42:24 +0300

    +scroll_bar

--- screens/RequestForm.js
+++ screens/RequestForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
-import { createRequest } from '../services/api';
+import { View, Text, TextInput, Button, FlatList, TouchableOpacity, StyleSheet, KeyboardAvoidingView, Platform, ScrollView, Keyboard } from 'react-native';
 import { TextInputMask } from 'react-native-masked-text';
 
 const RequestForm = ({ navigation }) => {
@@ -8,12 +7,12 @@ const RequestForm = ({ navigation }) => {
         subdivision: '',
         type: '',
         quantity: '',
-        timeSlots: [],
         plannedWorkTime: '',
         distance: '',
         master: '',
     });
     const [newTimeSlot, setNewTimeSlot] = useState('');
+    const [timeSlots, setTimeSlots] = useState([]);
 
     const handleInputChange = (name, value) => {
         setRequestData({ ...requestData, [name]: value });
@@ -22,18 +21,15 @@ const RequestForm = ({ navigation }) => {
     const addTimeSlot = () => {
         if (newTimeSlot) {
             const { type, quantity } = requestData;
-            setRequestData((prevData) => ({
-                ...prevData,
-                timeSlots: [...prevData.timeSlots, { time: newTimeSlot, type, quantity }],
-            }));
+            setTimeSlots((prevSlots) => [...prevSlots, { time: newTimeSlot, type, quantity }]);
             setNewTimeSlot('');
         }
     };
 
     const handleSubmit = async () => {
         try {
-            await createRequest(requestData);
-            navigation.navigate('RequestList'); // Вернуться к списку заявок
+            await createRequest({ ...requestData, timeSlots });
+            navigation.navigate('RequestList'); 
         } catch (error) {
             console.error('Ошибка при отправке заявки:', error);
         }
@@ -50,6 +46,8 @@ const RequestForm = ({ navigation }) => {
                         value={requestData[item.name]}
                         keyboardType={item.keyboardType}
                         placeholder={item.placeholder}
+                        returnKeyType="done"
+                        onSubmitEditing={Keyboard.dismiss}
                     />
                 </View>
             );
@@ -71,36 +69,46 @@ const RequestForm = ({ navigation }) => {
         { type: 'input', label: 'Плановое время работы', name: 'plannedWorkTime', placeholder: 'Введите плановое время работы' },
         { type: 'input', label: 'Расстояние до объекта (км)', name: 'distance', keyboardType: 'numeric' },
         { type: 'input', label: 'Мастер, подавший заявку', name: 'master' },
-        { type: 'timeSlot', time: newTimeSlot, type: requestData.type, quantity: requestData.quantity },
     ];
 
     return (
-        <View style={styles.container}>
-            <FlatList
-                data={data}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={renderItem}
-                ListFooterComponent={
-                    <View>
-                        <Text style={styles.label}>Время подачи:</Text>
-                        <TextInputMask
-                            type={'datetime'}
-                            options={{
-                                format: 'HH:mm',
-                            }}
-                            style={styles.input}
-                            value={newTimeSlot}
-                            onChangeText={setNewTimeSlot}
-                            placeholder="Введите время (HH:mm)"
-                        />
-                        <TouchableOpacity style={styles.addButton} onPress={addTimeSlot}>
-                            <Text style={styles.addButtonText}>Добавить время</Text>
-                        </TouchableOpacity>
-                        <Button title="Отправить заявку" onPress={handleSubmit} />
-                    </View>
-                }
-            />
-        </View>
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior={Platform.OS === 'android' ? 'height' : 'padding'}
+            keyboardVerticalOffset={Platform.OS === 'android' ? 100 : 0}
+        >
+            <ScrollView>
+                {data.map((item, index) => renderItem({ item }))}
+                <View>
+                    <Text style={styles.label}>Время подачи:</Text>
+                    <TextInputMask
+                        type={'datetime'}
+                        options={{
+                            format: 'HH:mm',
+                        }}
+                        style={styles.input}
+                        value={newTimeSlot}
+                        onChangeText={setNewTimeSlot}
+                        placeholder="Введите время (HH:mm)"
+                    />
+                    <TouchableOpacity style={styles.addButton} onPress={addTimeSlot}>
+                        <Text style={styles.addButtonText}>Добавить время</Text>
+                    </TouchableOpacity>
+                </View>
+                <FlatList
+                    data={timeSlots}
+                    renderItem={({ item }) => (
+                        <View style={styles.timeSlot}>
+                            <Text>{item.time}</Text>
+                            <Text style={styles.infoText}>Тип: {item.type}, Количество: {item.quantity}</Text>
+                        </View>
+                    )}
+                    keyExtractor={(item, index) => index.toString()}
+                />
+                
+                <Button title="Отправить заявку" onPress={handleSubmit} />
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 };
 
@@ -109,9 +117,6 @@ const styles = StyleSheet.create({
         padding: 20,
         backgroundColor: '#f8f8f8',
         flex: 1,
-    },
-    scrollViewContent: {
-        paddingBottom: 20,
     },
     label: {
         marginVertical: 10,
